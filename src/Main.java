1d93496950d89d3188040a3c24d93f09abd20c62
vitaliizuzka <vitaliizuzka@gmail.com>
2025-05-16 15:04:14 +0300

    main create person

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import model.Person;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("second");
         System.out.println("third");
+        Person person = new Person("Vasya", 15);
+        System.out.println(person);
     }
 }
