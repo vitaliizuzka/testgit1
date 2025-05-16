551cefe24181293e62c543c05987480f429fe016
vitaliizuzka <vitaliizuzka@gmail.com>
2025-05-16 15:00:28 +0300

    person set get

--- src/model/Person.java
+++ src/model/Person.java
@@ -11,4 +11,20 @@ public class Person {
 
     public Person() {
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
