ab966ba86c79d9c77bbdb2ce59a2af2c7ea66c3e
vitaliizuzka <vitaliizuzka@gmail.com>
2025-05-16 14:57:34 +0300

    main third

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("second");
+        System.out.println("third");
     }
 }
