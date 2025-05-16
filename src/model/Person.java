cb603d7929d92739f8812241965e604ca6a655c8
vitaliizuzka <vitaliizuzka@gmail.com>
2025-05-16 15:00:11 +0300

    person construct

--- src/model/Person.java
+++ src/model/Person.java
@@ -3,4 +3,12 @@ package model;
 public class Person {
     private String name;
     private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
 }
