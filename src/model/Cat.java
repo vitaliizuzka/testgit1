f5fd9360e9f6732197270371b8d6c003a8c1f12c
vitaliizuzka <vitaliizuzka@gmail.com>
2025-05-16 15:08:01 +0300

    cat toString

--- src/model/Cat.java
+++ src/model/Cat.java
@@ -2,4 +2,24 @@ package model;
 
 public class Cat {
     private String nickname;
+
+    public Cat(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "nickname='" + nickname + '\'' +
+                '}';
+    }
 }
