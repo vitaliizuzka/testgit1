b61897164f31d21064c5b2bb00c94854dbeec7e6
vitaliizuzka <vitaliizuzka@gmail.com>
2025-05-16 15:01:00 +0300

    person equals and hash

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Person {
     private String name;
     private int age;
@@ -26,5 +28,18 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 }
